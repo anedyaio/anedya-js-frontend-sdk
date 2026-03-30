7031856cfac0e61f7a399d3d75f9eb92ea177abb
abhishreecodes <bhishree18@gmail.com>
2026-03-30 11:04:11 +0530

    added streamns

--- examples/application-example.js
+++ examples/application-example.js
@@ -18,11 +18,11 @@ const {
 } = require("@anedyasystems/anedya-frontend-sdk");
 
 // Your Anedya credentials
-const tokenId = "itATCNCVamYNmwfdeBumQMwS";
-const token = "Evd8wL35Xt4tNQOXP11YddiRHtM74jqw3JZYuIaPmnWxNozbmnNTUfVaqXmQERV1";
+const tokenId = "iG91tS7YJ1h3NPU9gepjVrL8";
+const token = "7cLzCk3OVcDtVlCJbOBX4DyLWr67ny5ocVv528U2yoK6tenJXiudUJrfhbo1NDnh";
 const NodeId = "20deeee8-f8ae-11ee-9dd8-c3aa61afe2fb";
 const variableIdentifier = "temperature";
-const streamId="019d28ae-fb49-7f41-8307-f88a1b29505e"
+const streamId="019d2e20-655c-74af-ada3-8d4bba3a3305"
 const streamUrl="wss://ZxBpErVPCj.acs-r1.ap-in-1.anedya.io/v1/streams/connect"
 
 
